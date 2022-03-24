1747b804b0e039b981a887e5e846be2f4de95059
jonathancowling <jonathan.cowling@infinityworks.com>
2022-03-24 17:20:23 +0000

    Fix tests

--- ingester/app/src/test/java/com/infinityworks/cowling/jonathan/twipinion/ingester/TweetIngesterApplicationTests.java
+++ ingester/app/src/test/java/com/infinityworks/cowling/jonathan/twipinion/ingester/TweetIngesterApplicationTests.java
@@ -3,7 +3,11 @@ package com.infinityworks.cowling.jonathan.twipinion.ingester;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+	properties = {
+		"TWITTER_BEARER="
+	}
+)
 class TweetIngesterApplicationTests {
 
 	@Test
