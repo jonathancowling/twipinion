d45bb342f1143e9d3f75f33538c443a34df2e07c
jonathancowling <jonathan.cowling@infinityworks.com>
2022-04-06 15:57:12 +0100

    Fix public subnet

--- shared-network/inf/main.go
+++ shared-network/inf/main.go
@@ -106,7 +106,7 @@ func main() {
 
 		subnets := make([]pulumi.IDOutput, len(zoneNames))
 		for i, zone := range zoneNames {
-			subnetCidr, err := cidr.Subnet(network, 8, i)
+			subnetCidr, err := cidr.Subnet(network, 8, i + 1)
 			if err != nil {
 				return err
 			}
